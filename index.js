484de86b299e78f1637daa4a94a8b5d423dee723
abhik1990 <way2abhik@gmail.com>
2014-10-14 14:25:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var hobby="multiply";
+var status="try";
+console.log("loading multiply module");
+var timesTwo function(x){
+	return x*2;
+};
+var timesThree function(x){
+	return x*3;
+};
+export hobby=hobby;
+export.timesTwo=timesTwo;
+export.timesThree=timesThree;
